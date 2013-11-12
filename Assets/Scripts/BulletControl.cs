095d8ef6da1f28737397f07418ba806cba9225c9
Core <seorisachs@gmail.com>
2013-11-11 21:26:25 -0500

    bullets,  movement, updated 3D text

---
+++ Assets/Scripts/BulletControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletControl : MonoBehaviour {
+        
+        bool bulletBool = false;
+        float bulletTime;
+        
+        // Use this for initialization
+        void Start () {
+        
+        }
+        
+        // Update is called once per frame
+        void Update () {
+                if (bulletBool == false) {
+                        bulletTime = Time.time;
+                        bulletBool = true;
+                        //Debug.Log ("TimerStart");
+                }
+                if ((bulletBool == true) && ((bulletTime + 1.5f) < Time.time)) {
+                        //Debug.Log ("Destorying");
+                        transform.localScale -= new Vector3(0f, 1f, 0f);
+                        Destroy(gameObject);
+                }
+        }
+        
+        void OnTriggerEnter(Collider other) {
+                if (other.tag == "Wall") {
+                        //Debug.Log("Wall hit");
+                        Destroy (gameObject);
+                }
+                if (other.tag == "Enemy") {
+                        Debug.Log ("Supressing Fire!");
+                }
+        }
+}
---
+++ Assets/Scripts/TeslaRaygun.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeslaRaygun : MonoBehaviour {
+        
+        //float scaleTrack = 0f;
+        //float scaleBeam = 3f;
+        float bulletSpeed = 10f;
+        public Rigidbody bullet;
+        
+        // Use this for initialization
+        void Start () {
+        
+        }
+        
+        // Update is called once per frame
+        void Update () {
+                /*
+                Ray checkRay = new Ray(transform.position,transform.forward);
+                RaycastHit checkHit;
+                
+                if (Input.GetKey(KeyCode.Space)) {
+                        Debug.Log ("Firing");
+                        if (Physics.Raycast (checkRay, out checkHit, scaleBeam)) {
+                                Debug.Log ("Stopped");
+                        }
+                        if (Physics.Raycast(checkRay, out checkHit, scaleBeam) == false) {
+                                Debug.Log("Call Scale");
+                                transform.localScale += new Vector3(0f, 0f, 0.5f);
+                                scaleBeam += 0.5f;
+                                scaleTrack += 0.5f;
+                                Debug.Log("Scale");
+                        }
+                } else {
+                        if (scaleTrack >= -0.1f) {
+                                transform.localScale -= new Vector3(0f,0f,scaleTrack);
+                                scaleTrack -= 0.5f;
+                        }
+                }
+                */
+        }
+        
+        void FixedUpdate() {
+                if (Input.GetKey(KeyCode.Space)) {
+                        Rigidbody prefabBullet = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+                        prefabBullet.velocity = transform.TransformDirection(new Vector3 (0, 0, bulletSpeed));
+                }        
+        }
+}
