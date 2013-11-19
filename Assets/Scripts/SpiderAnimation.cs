5b5b2eda2fbb4dbf5f7fc1c34d4e80879b6d3947
Core <seorisachs@gmail.com>
2013-11-19 00:08:33 -0500

    spiders are in with code and killability

---
+++ Assets/Scripts/SpiderAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderAnimation : MonoBehaviour {
+	
+	public Rigidbody myRigidbody;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animation.Play("SpiderWalk");
+	
+	}
+}
