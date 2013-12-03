2a3ac8db51f114dad06ed8632bb4ec69759bed8f
Core <seorisachs@gmail.com>
2013-12-03 08:22:35 -0500

    some level design stuff but then realized that maybe i should try to get tesla moving. trying to work on that now.

---
+++ Assets/Scripts/SpiderDeath.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter() {
+		transform.position = new Vector3 (32f, 3f, 10f);
+	}
+}
